a44d0353b1c64929704e7bb96089bfa2827a6e2f
David Washington <dwashin@outlook.com>
2016-02-09 18:57:16 -0600

    initial checkin

---
+++ public/js/default.js
@@ -0,0 +1,32 @@
+﻿var menu;
+var headerItem;
+
+window.onload = function() {
+    headerItem = document.querySelector(".headerItem");  
+    menu = document.querySelector(".menu"); 
+    
+    headerItem.addEventListener("pointerdown", downHandler);
+    headerItem.addEventListener("pointerup", upHandler);
+    headerItem.addEventListener("pointerout", cancelHandler);
+    headerItem.addEventListener("pointercancel", cancelHandler);
+    headerItem.addEventListener("pointerover", downHandler);
+
+    menu.addEventListener("pointerover", downHandler);
+    menu.addEventListener("pointerout", cancelHandler);
+};
+
+function downHandler(eventObject) {
+    menu.classList.add("shown");
+}
+
+function upHandler(eventObject) {
+    if (eventObject.pointerType == "mouse") {
+        menu.classList.remove("shown");
+        alert("Header clicked");
+    }
+}
+
+function cancelHandler(eventObject) {
+    menu.classList.remove("shown");
+}
+
